db184cbbe0d6451a985d6bd812c4cfd88c0a82a4
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-06 12:42:25 +0530

    solved problems

---
+++ Trees/Balanced Binary Tree/Balanced Binary Tree.py
@@ -0,0 +1,22 @@
+#https://leetcode.com/problems/balanced-binary-tree/
+def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        balance = [True]
+        
+        def dfs(root):
+            
+            if root is None :
+                return balance[0]
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            if (abs(left-right)>1):
+                balance[0] = False
+                return balance[0]
+            
+            return max(left,right) + 1
+        
+        dfs(root)
+        
+        return balance[0]
