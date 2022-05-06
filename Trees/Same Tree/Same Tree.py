af0d8bb6460d3c492391f8dc4b3db5802011680c
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-06 12:52:04 +0530

    solved problems

---
+++ Trees/Same Tree/Same Tree.py
@@ -0,0 +1,11 @@
+
+def isSameTree(self, p, q):
+        
+        if not p and not q:
+            return True
+        
+        if (not p or not q) or (p.val!= q.val):
+            return False
+        
+        
+        return (self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right))
