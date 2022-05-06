83910ee870209bca6e663c3e5b75c086e7c74951
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-06 14:17:01 +0530

    solved problems

---
+++ Tries/Implement Trie/Implement Trie.py
@@ -0,0 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            
+            curr = curr.children[c]
+        
+        return curr.end
+
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        
+        return True
+
+
+
+# Your Trie object will be instantiated and called as such:
+obj = Trie()
+obj.insert("apple")
+param_2 = obj.search("apple")
+param_3 = obj.startsWith("app")
+
+print(param_2)
+print(param_3)
