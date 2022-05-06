1613ab2e19af5c3b122d38ebf266ee6a5a0c503c
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-06 18:34:05 +0530

    solved problems

---
+++ Heap_Priority Queue/K th largest element in Stream/K th Largest.py
@@ -0,0 +1,19 @@
+import heapq    
+"""
+https://leetcode.com/problems/kth-largest-element-in-a-stream/submissions/
+"""
+    
+def __init__(self, k, nums):
+    self.minHeap, self.k = nums, k
+    heapq.heapify(self.minHeap)
+    while(len(self.minHeap)>self.k):
+        heapq.heappop(self.minHeap)
+        
+
+def add(self, val):
+    heapq.heappush(self.minHeap,val)
+    if len(self.minHeap)>self.k:
+        heapq.heappop(self.minHeap)
+    return self.minHeap[0]
+
+    
