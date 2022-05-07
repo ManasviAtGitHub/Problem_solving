247399462fbeb28c659b300cddfdd48dc7528b1a
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-07 17:41:33 +0530

    solved problems

---
+++ 1-D DP/Climbing Stairs/Climbing Stairs.py
@@ -0,0 +1,16 @@
+
+def stairs(n):
+
+    if n==0:
+        return 0
+    step1, step2 = 1, 1
+
+    for i in range(n-1):
+        temp = step1
+        step1 = step1 + step2
+        step2 = temp
+
+    return step1
+
+
+print(stairs(5))
