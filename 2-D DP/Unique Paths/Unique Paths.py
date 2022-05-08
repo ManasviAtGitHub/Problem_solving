e5bc9a9d8544d29e1197e8c4ff53c61ff3a207ef
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-08 13:15:38 +0530

    solved problems

--- 2-D DP/Unique Paths/Unique Paths.py
+++ 2-D DP/Unique Paths/Unique Paths.py
@@ -1,2 +1,20 @@
+"""
+https://leetcode.com/problems/unique-paths/
+"""
+
 def uniquePaths(m,n):
-    
+    row = [1]*n
+
+    # as last row and colunns are always we use
+    # that fact to our advantage
+    for i in range(m-1):
+        newRow = [1]*n
+        
+        for j in range(n-2, -1, -1):
+            newRow[j] = newRow[j+1] + row[j]
+        
+        row = newRow
+
+    return row[0]
+
+print(uniquePaths(3,7))
