ce7e021fd22a1692622dc8babae87e88e34154d7
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-08 11:55:32 +0530

    solved problems

---
+++ Heap_Priority Queue/Last Stone Weight/Last stone weight.py
@@ -0,0 +1,21 @@
+import heapq
+
+def lastStone(stones):
+    stones = [-s for s in stones]
+    heapq.heapify(stones)
+
+    while len(stones) > 1:
+        first = heapq.heappop(stones)
+        second = heapq.heappop(stones)
+        # don't need to check for equal values as popping does the job
+        if second > first:
+            heapq.heappush(stones, first - second)
+    
+    # append 0 if no element is their then will return 0
+    stones.append(0)
+    return abs(stones[0])
+    
+
+
+
+print(lastStone(stones = [2,7,4,1,8,1]))
