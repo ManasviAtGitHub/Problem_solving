0dda4bb2962565a40b1519f5fe0cc92b4c9466b6
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-17 09:07:56 +0530

    solved problems

---
+++ 2-D DP/Longest Common Subsequence/Longest Common Subsequence.py
@@ -0,0 +1,14 @@
+def lcs(text1,text2):
+        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)] 
+        
+        for i in range(len(text1)-1,-1,-1):
+            for j in range(len(text2)-1,-1,-1):
+                
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+            
+        return dp[0][0]
+
+print(lcs("abcde","ace"))
