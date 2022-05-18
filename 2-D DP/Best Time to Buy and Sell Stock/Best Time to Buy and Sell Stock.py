040c1e6b99a68f411a42f7bbd2b0e73c1c5e58e6
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-18 17:43:11 +0530

    solved problems

---
+++ 2-D DP/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,26 @@
+def maxProfit(prices):
+        dp ={}
+        
+        def dfs(i,buying):
+            if i>=len(prices):
+                return 0
+        
+            if (i,buying) in dp:
+                return dp[(i,buying)]
+            
+            if buying:
+                buy = dfs(i+1, not buying) - prices[i]
+                cooldown = dfs(i+1,buying)
+                dp[(i,buying)] = max(buy, cooldown)
+                
+            else:
+                sell = dfs(i+2, not buying) + prices[i]
+                cooldown = dfs(i+1,buying)
+                dp[(i,buying)] = max (sell, cooldown)
+            return dp[(i,buying)]
+        
+        return dfs(0,True)
+
+
+
+print(maxProfit([1,2,3,0,2]))
