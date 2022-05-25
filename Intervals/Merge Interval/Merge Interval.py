009993abf7c89b4a69e78ceb0697896447abb195
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-25 14:58:32 +0530

    solved problems

---
+++ Intervals/Merge Interval/Merge Interval.py
@@ -0,0 +1,19 @@
+def merge(intervals):
+        
+        intervals.sort(key = lambda i :i[0])
+        res = [intervals[0]]
+        
+        for start, end in intervals[1:]:
+          
+          last = res[-1][1]
+          
+          
+          if start <=last:
+            res[-1][1] = max(last, end)
+          else:
+            res.append([start,end])
+          
+        return res
+
+print(merge(intervals = [[1,3],[2,6],[8,10],[15,18]]))
+
