6595ad0cf923c43c13a40106d76619af8789beae
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-05-30 19:06:13 +0530

    solved problems

---
+++ Stack/Car Fleet/Car Fleet.py
@@ -0,0 +1,18 @@
+"""
+https://leetcode.com/problems/car-fleet/
+"""
+
+def carFleet(target, position, speed) -> int:
+        pair = [[p,s] for p, s in zip(position, speed)]
+        
+        stack = []
+        for p,s in sorted(pair)[::-1]:
+          stack.append((target-p)/s)
+          if len(stack)>=2 and stack[-1] <= stack[-2]:
+            stack.pop()
+        
+        
+        return len(stack)
+
+
+print(carFleet(target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]))
