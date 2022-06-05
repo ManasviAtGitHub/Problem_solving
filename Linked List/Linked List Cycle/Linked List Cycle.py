e805515ea8ae251ed517bcb28cb92744dd95c24a
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-06-05 23:13:26 +0530

    solved problems

---
+++ Linked List/Linked List Cycle/Linked List Cycle.py
@@ -0,0 +1,19 @@
+"""
+https://leetcode.com/problems/linked-list-cycle
+"""
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        slow, fast = head, head
+        
+        while fast and fast.next:
+          
+          slow = slow.next
+          fast = fast.next.next
+          
+          if slow == fast:
+            return True
+          
+        
+        return False
