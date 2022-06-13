7fae39bfc56c6d3bbe61765af0c81281a180de60
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-06-13 14:42:20 +0530

    solved problems

---
+++ Graphs/Rotting_Oranges/Rotting Oranges.py
@@ -0,0 +1,41 @@
+"""
+https://leetcode.com/problems/rotting-oranges
+"""
+
+import collections
+
+
+def orangesRotting(grid):
+        q = collections.deque()
+        
+        time, fresh = 0, 0
+        R, C = len(grid), len(grid[0])
+        
+        for r in  range(R):
+          for c in range(C):
+            if grid[r][c] == 1:
+              fresh+=1
+            if grid[r][c] == 2:
+              q.append([r,c])
+        
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while q and fresh>0:
+          for i in range(len(q)):
+            r, c = q.popleft()
+            
+            for dr, dc in directions:
+              row, col = dr + r, dc + c
+              if (row<0 or row==R) or (col<0 or col==C) or (grid[row][col]!=1):
+                continue
+              
+              grid[row][col] = 2
+              q.append([row, col])
+              fresh-=1
+            
+          time+=1
+        
+        return time if fresh == 0 else -1
+
+
+print(orangesRotting(grid = [[2,1,1],[1,1,0],[0,1,1]]))
+print(orangesRotting(grid = [[2,1,1],[0,1,1],[1,0,1]]))
