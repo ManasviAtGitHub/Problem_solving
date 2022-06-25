8658558ae9de4630c8c5324f4ff607ccbbfdf3a1
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-06-25 22:18:08 +0530

    solved problems

---
+++ Graphs/Surrounded Regions/Surrounded Regions.py
@@ -0,0 +1,40 @@
+
+"""
+https://leetcode.com/problems/surrounded-regions
+"""
+
+
+def solve(board):
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        ROWS, COLS = len(board), len(board[0])
+        
+        def capture(r, c):
+          if (r<0 or c<0 or r==ROWS or c==COLS or board[r][c] != "O"):
+            return
+          
+          board[r][c] = "T"
+          capture(r+1, c)
+          capture(r-1, c)
+          capture(r, c+1)
+          capture(r, c-1)
+          
+        # Capture unsurrounded regions (O->T)
+        
+        for r in range(ROWS):
+          for c in range(COLS):
+            if(board[r][c]=="O" and (r in [0, ROWS-1] or c in [0, COLS-1])):
+              capture(r, c)
+        
+        # Convert O to X and T to O
+        
+        for r in range(ROWS):
+          for c in range(COLS):
+            if board[r][c] == "O":
+              board[r][c] = "X"
+              
+            elif board[r][c] == "T":
+              board[r][c] = "O"
+
+
