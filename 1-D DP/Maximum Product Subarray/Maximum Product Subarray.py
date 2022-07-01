dad9877268551da09ddb4e20b798881433aec727
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-07-01 21:54:51 +0530

    solved problems

---
+++ 1-D DP/Maximum Product Subarray/Maximum Product Subarray.py
@@ -0,0 +1,19 @@
+
+
+def maxProduct(nums):
+        
+        res = max(nums)
+        
+        curMin, curMax = 1, 1
+        
+        for n in nums:
+          temp = curMax
+          curMax = max (n*curMax, n*curMin, n)
+          curMin = min (temp*n, n*curMin, n)
+          
+          res = max(res, curMax)
+          
+        return res
+
+
+print(maxProduct(nums = [2,3,-2,4]))
