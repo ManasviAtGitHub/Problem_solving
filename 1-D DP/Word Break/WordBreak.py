4837f2f166a27f22807d31088ee88a6884940f6b
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-07-04 21:41:56 +0530

    solved problems

---
+++ 1-D DP/Word Break/WordBreak.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problems/word-break
+"""
+
+def wordBreak(s, wordDict):
+      
+      dp = [False] * (len(s) + 1)
+      dp[len(s)] = True
+      
+      for i in range(len(s)-1, -1, -1):
+        for w in wordDict:
+          if (i+len(w))<= len(s) and s[i:i+len(w)]==w:
+            dp[i] = dp[i + len(w)]
+            
+          if dp[i]:
+            break
+      
+      return dp[0]
+
+print(wordBreak("appleandapple", ["apple", "and"]))
