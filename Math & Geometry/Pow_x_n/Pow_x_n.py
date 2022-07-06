e8f3062e0530e4212ececca93d85d631546e2874
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-07-06 22:14:47 +0530

    solved problems

---
+++ Math & Geometry/Pow_x_n/Pow_x_n.py
@@ -0,0 +1,19 @@
+"""
+https://leetcode.com/problems/powx-n
+"""
+def myPow(x, n):
+      def helper(x, n):
+        if x == 0 : return 0
+        if n == 0 : return 1
+        
+        
+        res = helper(x*x, n//2);
+        
+        return x*res if n%2 else res
+      
+      res = helper(x, abs(n))
+      
+      return res if n>=0 else 1/res
+
+
+print(myPow(5,4))
