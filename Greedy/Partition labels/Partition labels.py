0a0e1f18a737a195b6302100ea0a43914c39011c
ManasviAtGitHub <manasvisharma821@gmail.com>
2022-07-13 23:50:58 +0530

    solved problems

---
+++ Greedy/Partition labels/Partition labels.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/partition-labels
+"""
+
+def partitionLabels(s):
+      lastIndex = {}
+      
+      for i, c in enumerate(s):
+        lastIndex[c] = i
+        
+      res = []
+      
+      size, end = 0, 0
+      
+      for i, c in enumerate(s):
+        size+=1
+        end = max(end, lastIndex[c])
+        
+        if i == end:
+          res.append(size)
+          size = 0
+        
+        
+      return res
+
+
+print(partitionLabels("ababcbacadefegdehijhklij"))
